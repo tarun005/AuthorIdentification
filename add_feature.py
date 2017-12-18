07df2d93938bdd6dba1f5056dcdfe9e4cee1a7a6
Tarun <tarun.05.kalluri@gmail.com>
2017-12-18 18:04:44 +0530

    New files added

---
+++ add_feature.py
@@ -0,0 +1,8 @@
+def add_feature(X, feature_to_add):
+    """
+    Returns sparse feature matrix with added feature.
+    feature_to_add can also be a list of features.
+    """
+    from scipy.sparse import csr_matrix, hstack
+    return hstack([X, csr_matrix(feature_to_add).T], 'csr')
+
